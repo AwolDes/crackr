03856fe960a21e837b885c6ecac34e342499ff24
Austin Wilshire <awoldes@gmail.com>
2018-10-10 16:26:44 +1000

    UPDATE: benchmark

--- crackr_test.go
+++ crackr_test.go
@@ -2,7 +2,8 @@ package main
 
 import "testing"
 
-// run using go test -bench=. -benchmem -count 3 -v -cpuprofile cpu.out -memprofile mem.mprof
+// run using
+// go test -bench=. -benchmem -count 5 -v -cpuprofile cpu.out
 // profile using go tool pprof cpu.out
 // useful func
 // top5
@@ -11,8 +12,8 @@ import "testing"
 func BenchmarkSingleDictSingleHash(b *testing.B) {
 	// run the Fib function b.N times
 	dictionary := "passwords.txt"
-	hash := "5f4dcc3b5aa765d61d8327deb882cf99"
-	hashes := "nil"
+	hash := "nil"
+	hashes := "hashes.txt"
 	for n := 0; n < b.N; n++ {
 		attackUsingSingleDictionary(&dictionary, &hash, &hashes)
 	}
