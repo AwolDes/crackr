0c6dfc8b15347e14704e03f3c4d86a7c06fa4cff
Austin Wilshire <awoldes@gmail.com>
2018-10-10 13:47:13 +1000

    ADD: profiling docs link

--- crackr_test.go
+++ crackr_test.go
@@ -7,6 +7,7 @@ import "testing"
 // useful func
 // top5
 // list funcName
+// more here https://blog.golang.org/profiling-go-programs
 func BenchmarkSingleDictSingleHash(b *testing.B) {
 	// run the Fib function b.N times
 	dictionary := "passwords.txt"
