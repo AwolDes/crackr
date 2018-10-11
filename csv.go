b114f7086f0480a2326069baa1bb3408956bc2f4
Austin Wilshire <awoldes@gmail.com>
2018-10-12 09:02:07 +1000

    PERFORMANCE: reduce IO operation

--- csv.go
+++ csv.go
@@ -5,31 +5,15 @@ import (
 	"os"
 )
 
-func getRows(file string) [][]string {
-	f, err := os.Open(file + ".csv")
-	checkError("Cannot open file", err)
-	defer f.Close()
-
-	rows, err := csv.NewReader(f).ReadAll()
-	checkError("Cannot get rows file: ", err)
-	return rows
-}
-
-func addNewRow(rows [][]string, newRow []string) [][]string {
-	newRows := append(rows, newRow)
-	return newRows
-}
-
-func writeChanges(file string, rows [][]string) {
-	f, err := os.OpenFile(file+".csv", os.O_WRONLY, os.ModeAppend)
+func writeChanges(file string, data []string) {
+	f, err := os.OpenFile(file+".csv", os.O_APPEND|os.O_WRONLY, os.ModeAppend)
 	checkError("Couldn't save changes: ", err)
-	err = csv.NewWriter(f).WriteAll(rows)
-	checkError("Could not write changes: ", err)
+	w := csv.NewWriter(f)
+	w.Write(data)
+	w.Flush()
 	f.Close()
 }
 
 func writeCSV(file string, data []string) {
-	rows := getRows(file)
-	rows = addNewRow(rows, data)
-	writeChanges(file, rows)
+	writeChanges(file, data)
 }
