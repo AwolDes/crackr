554ce2f8492b9dc66c3aa0fe8ce31687ca44a572
Austin Wilshire <awoldes@gmail.com>
2018-10-12 15:56:22 +1000

    FIX: hashing bug

--- crackr.go
+++ crackr.go
@@ -1,6 +1,6 @@
 package main
 
-var hashAlgorithms = map[string]func(string) string{
+var hashAlgorithmFuncs = map[string]func(string) string{
 	"sha1":     sha1Hash,
 	"md5":      md5Hash,
 	"sha256":   sha256Hash,
@@ -9,13 +9,13 @@ var hashAlgorithms = map[string]func(string) string{
 	"sha3_512": sha3_512Hash,
 }
 
+var hashAlgorithmOptions = []string{"sha1", "md5", "sha256", "sha512", "sha3_256", "sha3_512"}
+
 func getHash(hashType string, plaintext string) string {
 	// note: a map function is faster than a switch
 	// source: https://hashrocket.com/blog/posts/switch-vs-map-which-is-the-better-way-to-branch-in-go
-	for _, hashAlgorithm := range hashAlgorithms {
-		return hashAlgorithm(plaintext)
-	}
-	panic("Hash type not supported!")
+	hashAlgorithm := hashAlgorithmFuncs[hashType]
+	return hashAlgorithm(plaintext)
 }
 
 func checkFoundPasswords(foundPasswords *[]string, hashedPassword string) bool {
@@ -35,8 +35,7 @@ func foundPassword(password string, hashedPassword string, hashAlgorithim string
 
 func checkPassword(passwords []string, foundPasswords *[]string, hash string) {
 	// for each hash algorithm, check all passwords
-	hashAlgorithims := []string{"sha1", "md5", "sha256", "sha512", "sha3_256", "sha3_512"}
-	for _, hashAlgorithim := range hashAlgorithims {
+	for _, hashAlgorithim := range hashAlgorithmOptions {
 		for _, password := range passwords {
 			hashedPassword := getHash(hashAlgorithim, password)
 			if !checkFoundPasswords(foundPasswords, hashedPassword) {
